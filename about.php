04324a2167692512a8c75b83a3bacc3735479abe
Freddy <fsolis@csumb.edu>
2015-02-22 22:08:55 -0800

    updated text in about.php

--- about.php
+++ about.php
@@ -54,19 +54,21 @@
       <div>
             <div class="center">
                 <h1 id="greenText"><b>About</b></h1>
+                <h5>Learn, Teach, Succeed.</h5>
             </div>
             <br />
           
           <div><h3><b>What is InterviewQuest?</b></h3></div>
             <div class="indented">
-                <p>InterviewQuest is a site that aims to help facilitate studying for the dreaded Technical Interview. Often sample questions for technical interviews are hidden deep in web forums and answers are often worded arbitarily which usually leaves us more nervous and confused than when we started. InterviewQuest hopes to make studying easier by providing a variety of questions with simple answers in a mock 'interview style' test. </p>
+                <p>InterviewQuest is a site that aims to help facilitate studying for the dreaded Technical Interview. Often sample questions for technical interviews are hidden deep in web forums and answers are often worded arbitrarily which usually leaves us more nervous and confused than when we started. InterviewQuest hopes to make studying easier by providing a variety of questions with simple answers in a mock 'interview style' test. </p>
             </div>
           
-            <div><h3>How Does It Work?</h3></div>
+          <div><h3><b>How does it work?</b></h3></div>
             <div class="indented">
                 <p>InterviewQuest is a mock interview test. When you land on the home page. Click START! From here you will be asked to chose the desired topics you want to study, the level of difficulty the questions will fall under, lastly you will choose the number of questions you want to answer. That's It! Your questions will then show up. Don't worry too much if are unsure of the specific steps just do the best you can, if you forget to input something the site will let you know what that is.</p>
             </div>
-            <div><h3>What Topics Can I Study?</h3></div>
+          
+          <div><h3><b>What topics can I study?</b></h3></div>
             <div class="indented">
                 <h4>Currently Supported:</h4>
                 <div class="row" id="greenText">
@@ -85,12 +87,44 @@
                     <div class="col-sm-2"><h6>JavaScript</h6></div>
                     <div class="col-sm-2"><h6>Software Engineering</h6></div>
                 </div>
-                <div class="row"><div class="col-sm-12" id="center">And Many More! We're Just Trying To Conserve Space.</div></div>
+                <div class="row"><div class="col-sm-12" id="center">And many more! We're just trying to conserve space.</div></div>
             </div> 
             <div class="indented">
-                <h4>Don't See The Topic You Wanted?</h4>
+                <h4>Don't see the topic you wanted?</h4>
                 <p>Don't worry, topics are added constantly. Don't want to wait? <b>Sign Up</b> and add it yourself. InterviewQuest can be a great learning tool as well! </p>
             </div>
+          
+          <div><h3><b>How Can InterviewQuest Help Learning?</b></h3></div>
+          <div class="indented">
+            <p>As technology aficionados, we constantly have to learn the next best thing. However, retaining all new information can be quite a difficult thing. Goal driven learning is proven to help facilitate retention. Let InterviewQuest be your goal. When we start learning something new, as we progress through the topic, we find that little and often important things are forgotten. Try this: as you learn a new topic, write down questions and answers. Make these answers simple, as if trying to explain it to someone completely new to the topic. This will assure that you truly understand the answer as well as help make it easier for someone new to learn the answer. This way the byproduct of your learning will help someone new to the topic learn it themselves. After all as Albert Einstein put it 'If you can't explain it to a six year old, you don't understand it yourself.'</p>
+          </div>
+          
+          <div><h3><b>What are the Skill Levels? What do they Mean?</b></h3></div>
+          <div class="indented">
+            <p>The difficulty of question are ranked on a 1 to 4 basis. Where the first level (Entry Level) are the easiest of the questions and 4 (Senior Level) are the hardest. In most engineering positions most of the positions for begineers are Entry Level positions where as Senior Level positions are meant for those with plenty of experience. However, when you choose a higher difficulty you will recieve simpler questions because most technical interviews start off basic and work thier way up. Don't skip the basics, under the pressure of an interview you might just forget some of the most simple answers.</p>
+          </div>
+          
+          <div><h3><b>Origin</b></h3></div>
+          <div class="indented">
+            <p>InterviewQuest was created by a Software Engineer fresh out of college. He found that you can never really know exactly what the interviewer will ask you, you might prepare for something specific and be asked something trivial that you never thought of studying. He found this out the hard way. So naturally he took to the Internet to study interview questions so it would never happen again. However, the process of tracking down interview questions and answers proved to be a hassle. Forums stuffed with good questions but answers were often too hard to find or to confusing when found. This is where InterviewQuest comes in, instead of going from book to book, forum to forum just select your topics and get ready to answer, just like a real interview. If you don't know the answer, it will be provided, otherwise it will remain hidden to avoid spoilers. Technical interviews can be nerve racking, let's take some of the stress off.</p>
+          </div>
+          
+          <div><h3><b>What's to come?</b></h3></div>
+          <div class="indented">
+              <p>We are costently adding new features and topics to this site to make it your number one site for prepping for your next great job.</p>
+              <p>Features to come:</p>
+            <ul>
+                <li>New Topics</li>
+                <li>New Questions</li>
+                <li>Android App</li>
+                <li>Ability to report and downvote incorrect questions</li>
+                <li>Ability to save certain quizzes</li>
+                <li>Email updates</li>
+            </ul>
+            <p>Can't wait for a new feature? Come help make development faster join our github project.</p>
+          </div>
+          <div><h4>Have a suggestion? Contact the creator below!</h4></div>
+          
       </div> <!-- end skill level information -->
       
         <br /> <br />
